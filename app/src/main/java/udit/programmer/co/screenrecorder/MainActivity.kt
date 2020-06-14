1b2568d692a4e829515acbc7029f4e3b450b43cb
Udit Jain <jain30udit@gmail.com>
2020-06-15 02:29:51 +0530

    Full Code

---
+++ app/src/main/java/udit/programmer/co/screenrecorder/MainActivity.kt
@@ -0,0 +1,266 @@
+package udit.programmer.co.screenrecorder
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.MediaRecorder
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.util.DisplayMetrics
+import android.util.SparseIntArray
+import android.view.Surface
+import android.view.View
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.lang.StringBuilder
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var screenDensity: Int = 0
+    private var projectManager: MediaProjectionManager? = null
+    private var mediaProjection: MediaProjection? = null
+    private var virtualDisplay: VirtualDisplay? = null
+    private var mediaProjectionCallback: MediaProjectionCallback? = null
+    private var mediaRecorder: MediaRecorder? = null
+
+    internal var videoUri: String = ""
+
+    companion object {
+        private val REQUEST_CODE = 1000
+        private val REQUEST_PERMISSION = 1001
+        private var DISPLAY_WIDTH = 700
+        private var DISPLAY_HEIGHT = 1200
+        private val ORIENTATION = SparseIntArray()
+
+        init {
+            ORIENTATION.append(Surface.ROTATION_0, 90)
+            ORIENTATION.append(Surface.ROTATION_90, 0)
+            ORIENTATION.append(Surface.ROTATION_180, 270)
+            ORIENTATION.append(Surface.ROTATION_270, 180)
+        }
+
+    }
+
+    inner class MediaProjectionCallback : MediaProjection.Callback() {
+
+        override fun onStop() {
+            if (btn_toggle.isChecked) {
+                btn_toggle.isChecked = true
+                mediaRecorder!!.stop()
+                mediaRecorder!!.reset()
+            }
+            mediaProjection = null
+            stopScreenRecorder()
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        screenDensity = metrics.densityDpi
+        mediaRecorder = MediaRecorder()
+        projectManager =
+            getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+
+        DISPLAY_HEIGHT = metrics.heightPixels
+        DISPLAY_WIDTH = metrics.widthPixels
+
+        btn_toggle.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) + ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.RECORD_AUDIO
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.RECORD_AUDIO
+                    )
+                ) {
+                    btn_toggle.isChecked = true
+                    Snackbar.make(rootLayout, "Permissions", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("ENABLE") {
+                            ActivityCompat.requestPermissions(
+                                this,
+                                arrayOf(
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                    Manifest.permission.RECORD_AUDIO
+                                ), REQUEST_PERMISSION
+                            )
+                        }.show()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.RECORD_AUDIO
+                        ), REQUEST_PERMISSION
+                    )
+                }
+            } else {
+                startRecording(it)
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    startRecording(btn_toggle)
+                } else {
+                    btn_toggle.isChecked = true
+                    Snackbar.make(rootLayout, "Permissions", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("ENABLE") {
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_SETTINGS
+                            intent.addCategory(Intent.CATEGORY_DEFAULT)
+                            intent.data = Uri.parse("packege:$packageName")
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                            startActivity(intent)
+                        }.show()
+                }
+            }
+        }
+    }
+
+
+    private fun startRecording(view: View?) {
+        if ((view as ToggleButton) != null) {
+            initRecorder()
+            shareScreen()
+        } else {
+            mediaRecorder!!.stop()
+            mediaRecorder!!.reset()
+            stopScreenRecorder()
+
+            video_view.visibility = View.VISIBLE
+            video_view.setVideoURI(Uri.parse(videoUri))
+            video_view.start()
+        }
+    }
+
+    private fun shareScreen() {
+        if (mediaProjection != null) {
+            startActivityForResult(projectManager!!.createScreenCaptureIntent(), REQUEST_CODE)
+            return
+        }
+        virtualDisplay = createVirtualDisplay()
+        mediaRecorder!!.start()
+    }
+
+    private fun createVirtualDisplay(): VirtualDisplay? {
+        return mediaProjection!!.createVirtualDisplay(
+            "MainActivity",
+            DISPLAY_WIDTH,
+            DISPLAY_HEIGHT,
+            screenDensity,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+            mediaRecorder!!.surface,
+            null,
+            null
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode != REQUEST_CODE) return
+
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "Screen Cast Permission Denied", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mediaProjectionCallback = MediaProjectionCallback()
+        mediaProjection = projectManager!!.getMediaProjection(resultCode, data!!)
+        mediaProjection!!.registerCallback(mediaProjectionCallback, null)
+        virtualDisplay = createVirtualDisplay()
+        mediaRecorder!!.start()
+
+        return super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun initRecorder() {
+        try {
+
+            mediaRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mediaRecorder!!.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+            mediaRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+
+            videoUri =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                    .toString() +
+                        StringBuilder("/").append("CeasedMeteor_Record").append(
+                            SimpleDateFormat("dd-MM-yyyy-hh_mm_ss").format(
+                                Date()
+                            )
+                        ).append(".mp4").toString()
+
+            mediaRecorder!!.setOutputFile(videoUri)
+            mediaRecorder!!.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+            mediaRecorder!!.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+            mediaRecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            mediaRecorder!!.setVideoEncodingBitRate(512 * 1000)
+            mediaRecorder!!.setVideoFrameRate(30)
+
+            val rotation = windowManager.defaultDisplay.rotation
+            val orientation = ORIENTATION.get(rotation + 50)
+            mediaRecorder!!.setOrientationHint(orientation)
+            mediaRecorder!!.prepare()
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun stopScreenRecorder() {
+        if (virtualDisplay == null) return
+
+        virtualDisplay!!.release()
+        destroyMediaProjection()
+    }
+
+    private fun destroyMediaProjection() {
+        if (mediaProjection != null) {
+            mediaProjection!!.unregisterCallback(mediaProjectionCallback)
+            mediaProjection!!.stop()
+            mediaProjection = null
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        destroyMediaProjection()
+    }
+
+}
